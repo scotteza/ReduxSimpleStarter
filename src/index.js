6e0ad6725ccc0142d99c00ef5f22849f945150ca
Scott Edwards <scott@hudl.cloud>
2018-01-16 20:45:32 +0200

    Returning to basics for Redux learning - emptying the project out.

--- src/index.js
+++ src/index.js
@@ -1,49 +1,11 @@
-import _ from "lodash";
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import YTSearch from "youtube-api-search";
-import SearchBar from "./components/search_bar";
-import VideoList from "./components/video_list";
-import VideoDetail from "./components/video_detail";
-const API_KEY = "AIzaSyCFC62t1CF-TNZ_0XQ52-DsgwnANni07iM";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './components/app';
+import store from './store';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      videos: [],
-      selectedVideo: null
-    };
-
-    this.videoSearch("surfboards");
-  }
-
-  videoSearch(term) {
-    YTSearch({ key: API_KEY, term: term }, videos => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0]
-      });
-    });
-  }
-
-  render() {
-    const videoSearch = _.debounce(term => {
-      this.videoSearch(term);
-    }, 300);
-
-    return (
-      <div>
-        <SearchBar onSearchTermChanged={videoSearch} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
-          videos={this.state.videos}
-        />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector(".container"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
