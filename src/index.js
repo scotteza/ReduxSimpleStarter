96c3aadba85cc7644d7bf5ce3d35144ac72e1757
Scott Edwards <scotteza@gmail.com>
2018-01-17 15:12:53 +0200

    Getting base app to base state.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './components/app';
-import store from './store';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
   </Provider>
   , document.querySelector('.container'));
--- src/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-export default createStoreWithMiddleware(reducers);
