bddc80b757a4534cef1a58ce6532a875ead42cdd
Scott Edwards <scotteza@gmail.com>
2018-01-18 21:15:19 +0200

    Initial weather code from tutorial.

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props) {
         super(props);
 
@@ -19,9 +22,16 @@ export default class SearchBar extends Component {
         this.setState({ term: event.target.props });
     }
 
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        // Go and get weather data
+    }
+
     render() {
         return (
-            <form className="input-group">
+            <form onSubmit={this.onFormSubmit}
+                className="input-group">
                 <input
                     placeholder="Get a five-day forecast in your favourite cities"
                     className="form-control"
@@ -35,3 +45,9 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
